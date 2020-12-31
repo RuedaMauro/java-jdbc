424e6b790cd815bfe0c50c79b657fe949e855ded
rubennicolasrueda <rubennicolasrueda@gmail.com>
2020-12-31 18:46:39 -0300

    postgres changes

---
+++ src/main/java/Program.java
@@ -0,0 +1,18 @@
+import sqlmanager.PostgreSQLConnection;
+import sqlmanager.SQLConnection;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Program {
+
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+        SQLConnection sql = new PostgreSQLConnection();
+        ResultSet result = sql.statementExecuteQuery("SELECT * FROM clientes.clientes.clientes");
+
+        while(result.next())
+        {
+            System.out.println(result.getString(3));
+        }
+    }
+}
