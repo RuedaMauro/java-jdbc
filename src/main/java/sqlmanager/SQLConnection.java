9db05e71da2e07ceb01850db83f0680f39944b9d
rubennicolasrueda <rubennicolasrueda@gmail.com>
2021-01-01 22:40:31 -0300

    adding sql exceptions and print table

--- src/main/java/sqlmanager/SQLConnection.java
+++ src/main/java/sqlmanager/SQLConnection.java
@@ -10,15 +10,24 @@ public class SQLConnection
     protected static Connection con;
 
     protected void getConnection(String url, String user, String password) throws SQLException, IOException {
-        Properties properties = new Properties();
-        FileInputStream fis = new FileInputStream("./src/main/resources/config.properties");
-        properties.load(fis);
-        con = DriverManager.getConnection(properties.getProperty(url), user, password);
+            Properties properties = new Properties();
+            FileInputStream fis = new FileInputStream("./src/main/resources/config.properties");
+            properties.load(fis);
+            con = DriverManager.getConnection(properties.getProperty(url), user, password);
     }
 
     public ResultSet statementExecuteQuery(String query) throws SQLException {
-        Statement statement = con.createStatement();
-        ResultSet resultSet = statement.executeQuery(query);
+        ResultSet resultSet = null;
+        if (con != null) {
+            Statement statement = con.createStatement();
+             resultSet = statement.executeQuery(query);
+        }
         return resultSet;
+    }
+
+    public void closeConnection() throws SQLException {
+        if (con != null){
+            con.close();
+        }
     }
 }
